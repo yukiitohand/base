85e0d008acd97125c8e9f287a332a82a1ee283a5
Yuki Itoh <yukiitoh@1x-nat-vl931-172-30-152-226.wireless.umass.edu>
2017-05-09 21:36:13 -0400

    expand_struct is added

---
+++ expand_struct.m
@@ -0,0 +1,57 @@
+function [ struct_new ] = expand_struct( varargin )
+%[ struct_concatenated ] = expand_struct( varargin )
+%   Expand struct having different fields
+%  Usage
+%    [ struct_new ] = expand_struct( struct1,struct2,struct3,... )
+
+switch length(varargin)
+    case 0
+        error('Empty input');
+    case 1
+        error('No need to concatenate');
+    otherwise
+end
+
+structList = varargin;
+
+allfieldnames = {};
+L = 0;
+for i=1:length(structList)
+    struct_cur = structList{i};
+    allfields_cur = fieldnames(struct_cur);
+    allfieldnames = union(allfieldnames,allfields_cur);
+    L = L + length(struct_cur);
+end
+
+struct_new = struct(allfieldnames{1},cell(1,L));
+for i=1:length(allfieldnames)
+    [struct_new.(allfieldnames{i})] = deal([]);
+end
+
+cumj = 0;
+for i=1:length(structList)
+    struct_cur = structList{i};
+    s = length(struct_cur);
+    allfields_cur = fieldnames(struct_cur);
+    for j=1:length(allfields_cur)
+        fld = allfields_cur{j};
+        for k=1:s
+            struct_new(k+cumj).(fld) = struct_cur(k).(fld);
+        end
+        % initialize the other using automatic estimation of the data type
+        % of each field.
+        setc = setdiff(1:L,cumj+1:cumj+s); 
+        idx_isempty = cellfun(@(x) isempty(x), [{struct_new(setc).(fld)}]);
+        repr = struct_new(cumj+1).(allfields_cur{j});
+        if isstr(repr)
+            [struct_new(setc(idx_isempty)).(fld)] = deal('');
+        elseif isscalar(repr)
+            [struct_new(setc(idx_isempty)).(fld)] = deal(NaN);
+        end
+        
+    end
+    cumj = cumj + s;
+end
+
+end
+
