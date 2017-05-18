38d5f9dc0bee253c12fc1f70e20dd7426e47da53
Yuki Itoh <yukiitoh@1x-nat-vl931-172-30-158-27.wireless.umass.edu>
2017-05-18 01:12:42 -0400

    support empty input

--- merge_struct.m
+++ merge_struct.m
@@ -3,6 +3,7 @@ function [ struct_new ] = merge_struct( varargin )
 %   Expand struct having different fields
 %  Usage
 %    [ struct_new ] = expand_struct( struct1,struct2,struct3,... )
+%    empty struct can be an input.
 
 switch length(varargin)
     case 0
@@ -18,9 +19,11 @@ allfieldnames = {};
 L = 0;
 for i=1:length(structList)
     struct_cur = structList{i};
-    allfields_cur = fieldnames(struct_cur);
-    allfieldnames = union(allfieldnames,allfields_cur);
-    L = L + length(struct_cur);
+    if ~isempty(struct_cur)
+        allfields_cur = fieldnames(struct_cur);
+        allfieldnames = union(allfieldnames,allfields_cur);
+        L = L + length(struct_cur);
+    end
 end
 
 struct_new = struct(allfieldnames{1},cell(1,L));
@@ -31,26 +34,28 @@ end
 cumj = 0;
 for i=1:length(structList)
     struct_cur = structList{i};
-    s = length(struct_cur);
-    allfields_cur = fieldnames(struct_cur);
-    for j=1:length(allfields_cur)
-        fld = allfields_cur{j};
-        for k=1:s
-            struct_new(k+cumj).(fld) = struct_cur(k).(fld);
+    if ~isempty(struct_cur)
+        s = length(struct_cur);
+        allfields_cur = fieldnames(struct_cur);
+        for j=1:length(allfields_cur)
+            fld = allfields_cur{j};
+            for k=1:s
+                struct_new(k+cumj).(fld) = struct_cur(k).(fld);
+            end
+            % initialize the other using automatic estimation of the data type
+            % of each field.
+            setc = setdiff(1:L,cumj+1:cumj+s); 
+            idx_isempty = cellfun(@(x) isempty(x), [{struct_new(setc).(fld)}]);
+            repr = struct_new(cumj+1).(allfields_cur{j});
+            if isstr(repr)
+                [struct_new(setc(idx_isempty)).(fld)] = deal('');
+            elseif isscalar(repr)
+                [struct_new(setc(idx_isempty)).(fld)] = deal(NaN);
+            end
+
         end
-        % initialize the other using automatic estimation of the data type
-        % of each field.
-        setc = setdiff(1:L,cumj+1:cumj+s); 
-        idx_isempty = cellfun(@(x) isempty(x), [{struct_new(setc).(fld)}]);
-        repr = struct_new(cumj+1).(allfields_cur{j});
-        if isstr(repr)
-            [struct_new(setc(idx_isempty)).(fld)] = deal('');
-        elseif isscalar(repr)
-            [struct_new(setc(idx_isempty)).(fld)] = deal(NaN);
-        end
-        
+        cumj = cumj + s;
     end
-    cumj = cumj + s;
 end
 
 end
