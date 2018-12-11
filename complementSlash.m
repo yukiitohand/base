9d691d1afcb85b30902681e53552a8dda2b3c6d3
Yuki Itoh <yukiitoh@Yukis-MacBook-Pro.local>
2018-12-11 14:22:59 -0500

    bug fixed

--- complementSlash.m
+++ complementSlash.m
@@ -2,7 +2,7 @@ function [pnew] = complementSlash(p)
 % [pnew] = complementSlash(p)
 %   add '/' at the end if there is not
 
-if ~strcmp(length(p),'/')
+if ~strcmp(p(length(p)),'/')
     pnew = [p '/'];
 else
     pnew = p;
