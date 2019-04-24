52311963c75736d65d310b2fe76902e455cb2b0f
Yuki Itoh <yukiitohand@gmail.com>
2019-04-24 13:29:01 -0400

    empty input is supported

--- complementSlash.m
+++ complementSlash.m
@@ -2,11 +2,14 @@ function [pnew] = complementSlash(p)
 % [pnew] = complementSlash(p)
 %   add '/' at the end if there is not
 
-if ~strcmp(p(length(p)),'/')
-    pnew = [p '/'];
-else
+if isempty(p)
     pnew = p;
+else
+    if ~strcmp(p(length(p)),'/')
+        pnew = [p '/'];
+    else
+        pnew = p;
+    end
 end
-    
 
 end
