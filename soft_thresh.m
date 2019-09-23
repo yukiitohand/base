9c13e17b59b28d4fae018808217e9fb6ceef451d
Yuki Itoh <yukiitohand@gmail.com>
2019-09-23 13:44:33 -0400

    the code is faster without verlessthan

--- soft_thresh.m
+++ soft_thresh.m
@@ -1,16 +1,23 @@
 function y = soft_thresh(x,T)
 
-if ~isscalar(T)
-    if size(x,1)==size(T,1) || size(x,2)==size(T,2)
-        
-    else
-        error('The size of inputs does not match.');
-    end
+y = sign(x) .* max(abs(x) - T, 0);
+
+%y = max(abs(x+T/2) - T/2, 0);
+%y = y./(y+T/2) .* (x+T/2);
+
 end
 
-if verLessThan('matlab','9.1')
-    y = sign(x) .* max(bsxfun(@minus,abs(x), T), 0);
-else
-    y = sign(x) .* max(abs(x) - T, 0);
-end
+% if ~isscalar(T)
+%     if size(x,1)==size(T,1) || size(x,2)==size(T,2)
+%         
+%     else
+%         error('The size of inputs does not match.');
+%     end
+% end
+% 
+% if verLessThan('matlab','9.1')
+%     y = sign(x) .* max(bsxfun(@minus,abs(x), T), 0);
+% else
+%     y = sign(x) .* max(abs(x) - T, 0);
+% end
 
