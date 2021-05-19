351bdc0459f0d3ab571fe2d394abca60fe457b92
Yuki Itoh <yukiitohand@gmail.com>
2021-05-19 01:31:47 -0400

    comments are added

--- chmod777.m
+++ chmod777.m
@@ -1,9 +1,12 @@
-function [] = chmod777(fpath,verbose)
-% [] = chmod777(fpath,verbose)
+function [status,msg] = chmod777(fpath,verbose)
+% [status,msg] = chmod777(fpath,verbose)
 %  Change the permission of the file to 777. Only works with Unix machine. 
 %  INPUTS
 %   fpath: path to the file or directory
 %   verbose: Print the result or not.
+%  OUTPUTS
+%   status: same as the output of fileattrib
+%   msg   : same as the output of fileattrib
 
 [status,msg] = fileattrib(fpath, '+w +x','a');
 if status
@@ -15,3 +18,12 @@ else
 end
 
 end
+
+% Following is the more raw command. Problem is that you do not know the
+% chmod succeeded or not.
+%
+% if isunix
+%     system(['chmod 777 ' fpath]);
+%     if verbose, fprintf('"%s": permission is set to 777.\n',fpath); end
+% end
+% 
