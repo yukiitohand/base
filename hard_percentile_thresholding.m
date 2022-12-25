93a10777c2df11946c3ec169a2a5f711c47a2b9c
Yuki Itoh <yuki.itoh@jhuapl.edu>
2022-12-25 02:18:52 -0500

    improvement

--- hard_percentile_thresholding.m
+++ hard_percentile_thresholding.m
@@ -1,40 +1,83 @@
-function [ ar_thed,lowhigh ] = hard_percentile_thresholding( ar,tol )
+function [ ar_thed,lowhigh,is_extrm_mask ] = hard_percentile_thresholding( ar,tol )
 % round lowest and highest (tol)*100 [%] values in its percentile
 % Inputs
 %    ar: array
-%    tol: percentile tolerance
+%    tol: scalar (<1.0) or 1x2 array (tol(1)+tol(2)<1.0). Percentile tolerance. 
+%       with numel(tol) = 1
+%          Extremeness of values is evaluated by the absolute deviation 
+%          from the median value of the array. The deviation is sorted by
+%          the deviation and the values larger than tol*100 percentile are 
+%          rounded up/down, depending on high or low extremness.
+%       with numel(tol) = 2,
+%          Values smaller than that of tol(1)*100 percentile are rounded up
+%          to that of tol(1)*100 percentile
+%          Values larger than that of (1-tol(2))*100 percentile are rounded
+%          down to that of (1-tol(2))*100 percentile
 % Outputs
-%    ar_thed: rounded array
+%    ar_thed: rounded array, same size as ar.
+%    lowhigh: 1 x 2 array, the first element is the minimum and the second
+%    element is the maximum of the ar_thed.
+%    is_extrm_mask: boolean, same size as ar. True if a value has an 
+%        extreme value, false otherwise.
 
-if isscalar(tol)
-    tol = [tol tol];
-end
-
-if sum(tol) > 1.0
-    error('Tolis too large.');
-end
+if sum(tol) > 1.0, error('Tolis too large.'); end
 
 ar2 = ar(:);
-[ V,I,R ] = nansort1d(ar2,'ascend');
+
+if isscalar(tol)
+    ar2_med = median(ar2,'omitnan');
+    [ V,I,R ] = nansort1d(abs(ar2-ar2_med),'ascend');
+elseif numel(tol) == 2
+    [ V,I,R ] = nansort1d(ar2,'ascend');
+else
+    error('The shape of tol is not right.');
+end
+
 if isempty(I)
     ar_thed = ar;
+    is_extrm_mask = false(size(ar));
     lowhigh = nan([1,2]);
-else
+elseif isscalar(tol)
+    is_extrm_mask = false(size(ar2));
+    N = length(I);
+    high_idx = ceil(N*(1-tol)+0.5);
+    % if high_idx>N, high_idx = N; end
+    
+    indx_extrm = I(high_idx:end);
+    is_extrm_indx_high = (ar2(indx_extrm)-ar2_med) > 0;
+    indx_high_extrm = indx_extrm( is_extrm_indx_high );
+    indx_low_extrm  = indx_extrm( ~is_extrm_indx_high );
+    
+    ar2(indx_extrm) = nan;
+    low  = min(ar2,[],1,'omitnan');
+    high = max(ar2,[],1,'omitnan');
+    
+    ar2(indx_low_extrm) = low;
+    ar2(indx_high_extrm) = high;
+    is_extrm_mask(indx_extrm) = true;
+    
+    ar_thed = reshape(ar2,size(ar));
+    is_extrm_mask = reshape(is_extrm_mask,size(ar));
+    
+    lowhigh = [low high];
+    
+elseif numel(tol) == 2
+    is_extrm_mask = false(size(ar2));
     N = length(I);
 
     low_idx = ceil(N*tol(1));
     high_idx = floor(N*(1-tol(2)));
-    if low_idx<1
-        low_idx = 1;
-    end
-    ar2(I(1:low_idx)) = V(low_idx);
     
-    if high_idx>N
-        high_idx = N;
-    end
+    if low_idx<1, low_idx = 1; end
+    ar2(I(1:low_idx)) = V(low_idx);
+    is_extrm_mask(I(1:low_idx)) = true;
+    
+    if high_idx>N, high_idx = N; end
     ar2(I(high_idx:end)) = V(high_idx);
+    is_extrm_mask(I(high_idx:end)) = true;
 
     ar_thed = reshape(ar2,size(ar));
+    is_extrm_mask = reshape(is_extrm_mask,size(ar));
     
     lowhigh = [V(low_idx) V(high_idx)];
     
--- im_hard_percentile_thresholding.m
+++ im_hard_percentile_thresholding.m
@@ -1,23 +1,28 @@
-function [ im_stretched,lowhigh ] = im_hard_percentile_thresholding( im,tol )
+function [ im_stretched,lowhigh,is_extrm_mask ] = im_hard_percentile_thresholding( im,tol )
 % [ im_stretched ] = im_hard_percentile_thresholding( im,tol )
 %   round lowest and highest (tol)*100 [%] values in its percentile channel
 %   by channel
 %   Inputs
 %     im: image [M x N x 1] or [M x N x 3]
-%     tol:
+%     tol: scalar or 1 x 2 array.
 %   Outputs
 %     im_stretched: same size 
+%     lowhigh: [1 x 2] or [3 x 2], each row shows the minimum and maximum
+%     value in the stretched image.
+%     is_extrm_mask: [M x N x 1] or [M x N x 3], boolean, true if an image
+%     cell has an extreme value, false otherwise.
 
 im = squeeze(im);
 
 if ismatrix(im)
-    [im_stretched,lowhigh] = hard_percentile_thresholding( im,tol );
+    [im_stretched,lowhigh,is_extrm_mask] = hard_percentile_thresholding( im,tol );
 elseif size(im,3)==3
-    [ r,lowhigh_r ] = hard_percentile_thresholding( im(:,:,1),tol );
-    [ g,lowhigh_g ] = hard_percentile_thresholding( im(:,:,2),tol );
-    [ b,lowhigh_b ] = hard_percentile_thresholding( im(:,:,3),tol );
+    [ r,lowhigh_r,is_extrm_mask_r ] = hard_percentile_thresholding( im(:,:,1),tol );
+    [ g,lowhigh_g,is_extrm_mask_g ] = hard_percentile_thresholding( im(:,:,2),tol );
+    [ b,lowhigh_b,is_extrm_mask_b ] = hard_percentile_thresholding( im(:,:,3),tol );
     im_stretched = cat(3,r,g,b);
     lowhigh = [lowhigh_r;lowhigh_g;lowhigh_b];
+    is_extrm_mask = cat(3,is_extrm_mask_r,is_extrm_mask_g,is_extrm_mask_b);
 else
     error('The size of the input image is invalid');
 end
